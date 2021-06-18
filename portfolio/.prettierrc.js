1fa802c9e397ba6a1dab47e42bc9e2c1be820d40
Jacob Dykstra <JDykstra@redwoodcu.org>
2021-06-18 11:11:43 -0700

    Feat: ESLINT implemented

---
+++ portfolio/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  endOfLine:"auto",
+  trailingComma: "all",
+  singleQuote: true,
+  jsxSingleQuote: true,
+  endOfLine: "auto",
+  printWidth: 120,
+  tabWidth: 4,
+};
