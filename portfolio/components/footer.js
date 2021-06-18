fa76934f408566ca0b67325c62ddd83a7a14b5db
Jacob Dykstra <JDykstra@redwoodcu.org>
2021-06-18 11:12:14 -0700

    Fix: Pages updated for routing

--- portfolio/components/footer.js
+++ portfolio/components/footer.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
-import signature from '../assets/Signature.png'
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import signature from "../assets/Signature.png";
 
-export class Footer extends Component {
-  whichQuote = () => {
-    switch (this.props.location.pathname) {
-      case "/bio":
-        return "Order and simplification are the first steps toward the mastery of a subject.";
-      case "/work":
-        return "We are what we repeatedly do. Excellence is not an act, but a habit."
-      case "/":
-        return "The future is now. It’s just not evenly distributed yet."
-      case "/languages":
-        return "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it."
-      case "/contact":
-        return "On the Internet, nobody knows you're a dog."
-      case "/blog":
-        return "/Because someone has to do it differently."
-      default:
-        break;
-    }
+const getQuote = (pathname) => {
+  switch (pathname) {
+    case "/bio":
+      return "Order and simplification are the first steps toward the mastery of a subject.";
+    case "/work":
+      return "We are what we repeatedly do. Excellence is not an act, but a habit.";
+    case "/":
+      return "The future is now. It’s just not evenly distributed yet.";
+    case "/languages":
+      return "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.";
+    case "/contact":
+      return "On the Internet, nobody knows you're a dog.";
+    case "/blog":
+      return "/Because someone has to do it differently.";
+    default:
+      break;
   }
+};
 
-  render() {
-    return (
-        <footer className="footer">
-          <div className="quote">{this.whichQuote()}</div>
-          <img className="signature" src={signature} alt="Jake's Signature"></img>
-        </footer>
-    );
-  }
-}
+export const Footer = () => {
+  const router = useRouter();
+
+  return (
+    <footer className="footer">
+      <div className="quote">{getQuote(router.pathname)}</div>
+      <Image className="signature" src={signature} alt="Jake's Signature" />
+    </footer>
+  );
+};
